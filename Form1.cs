b284e2079b0e2ed20620e5abcd293913c23c319e
Murashko Vladislav <vladislavuh2000@gmail.com>
2023-09-28 20:14:58 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,22 @@
+namespace Running_Button
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Мы и не сомневались, что Вы так думаете!");
+        }
+
+        private void mouseRun(object sender, MouseEventArgs e)
+        {
+            buttonNo.Top -= e.Y;
+            buttonNo.Left -= e.Y;
+            if (buttonNo.Top < -10 || buttonNo.Top > 100) buttonNo.Top = 60;
+            if (buttonNo.Left < -80 || buttonNo.Left > 250) buttonNo.Left = 120;
+        }
+    }
+}

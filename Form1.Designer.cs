3241580fb582923796fdcf355a7be0891686ff65
Murashko Vladislav <vladislavuh2000@gmail.com>
2023-09-30 15:33:15 +0300

    the fifth task was completed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Static = new Label();
             SuspendLayout();
+            // 
+            // Static
+            // 
+            Static.AutoSize = true;
+            Static.BackColor = SystemColors.ControlText;
+            Static.Font = new Font("Segoe UI", 24F, FontStyle.Italic, GraphicsUnit.Point);
+            Static.ForeColor = Color.Snow;
+            Static.Location = new Point(340, 176);
+            Static.Name = "Static";
+            Static.Size = new Size(95, 45);
+            Static.TabIndex = 0;
+            Static.Text = "Static";
+            Static.MouseMove += LabelMouseMove;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(Static);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Running Static";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label Static;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,19 @@ namespace Running_Button
         {
             InitializeComponent();
         }
+
+        private void LabelMouseMove(object sender, MouseEventArgs e)
+        {
+            Random rand = new Random(DateTime.Now.Millisecond);
+            if (Static.Location.X > Width / 2)
+            {
+                Static.Location = new Point(rand.Next(0+Static.Width+30, Width / 2),rand.Next(30+ Static.Height,Height- Static.Height-30));
+            }
+            else
+            {
+                Static.PointToScreen(Static.Location);
+                Static.Location = new Point(rand.Next(Width/2, Width-Static.Width-30), rand.Next(30+ Static.Height,Height- Static.Height-30));
+            }
+        }
     }
 }

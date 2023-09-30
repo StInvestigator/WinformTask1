73119a838850fd29008d44b6be8de7dc520c434e
Murashko Vladislav <vladislavuh2000@gmail.com>
2023-09-30 17:12:46 +0300

    The seventh task was completed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            textBox = new TextBox();
+            YearsButton = new RadioButton();
+            MonthsButton = new RadioButton();
+            DaysButton = new RadioButton();
+            MinutesButton = new RadioButton();
+            SecondsButton = new RadioButton();
+            label2 = new Label();
+            label3 = new Label();
+            ResultLabel = new Label();
+            label4 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(104, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(301, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Enter the date in the future.";
+            // 
+            // textBox
+            // 
+            textBox.Location = new Point(104, 102);
+            textBox.Name = "textBox";
+            textBox.Size = new Size(436, 23);
+            textBox.TabIndex = 1;
+            textBox.TextChanged += TextChanged;
+            // 
+            // YearsButton
+            // 
+            YearsButton.AutoSize = true;
+            YearsButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            YearsButton.Location = new Point(104, 226);
+            YearsButton.Name = "YearsButton";
+            YearsButton.Size = new Size(75, 29);
+            YearsButton.TabIndex = 2;
+            YearsButton.TabStop = true;
+            YearsButton.Text = "Years";
+            YearsButton.UseVisualStyleBackColor = true;
+            YearsButton.CheckedChanged += YearsCheckedChange;
+            // 
+            // MonthsButton
+            // 
+            MonthsButton.AutoSize = true;
+            MonthsButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            MonthsButton.Location = new Point(185, 226);
+            MonthsButton.Name = "MonthsButton";
+            MonthsButton.Size = new Size(92, 29);
+            MonthsButton.TabIndex = 3;
+            MonthsButton.TabStop = true;
+            MonthsButton.Text = "Months";
+            MonthsButton.UseVisualStyleBackColor = true;
+            MonthsButton.CheckedChanged += MonthsCheckedChange;
+            // 
+            // DaysButton
+            // 
+            DaysButton.AutoSize = true;
+            DaysButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            DaysButton.Location = new Point(283, 226);
+            DaysButton.Name = "DaysButton";
+            DaysButton.Size = new Size(71, 29);
+            DaysButton.TabIndex = 4;
+            DaysButton.TabStop = true;
+            DaysButton.Text = "Days";
+            DaysButton.UseVisualStyleBackColor = true;
+            DaysButton.CheckedChanged += DaysCheckedChange;
+            // 
+            // MinutesButton
+            // 
+            MinutesButton.AutoSize = true;
+            MinutesButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            MinutesButton.Location = new Point(360, 226);
+            MinutesButton.Name = "MinutesButton";
+            MinutesButton.Size = new Size(96, 29);
+            MinutesButton.TabIndex = 5;
+            MinutesButton.TabStop = true;
+            MinutesButton.Text = "Minutes";
+            MinutesButton.UseVisualStyleBackColor = true;
+            MinutesButton.CheckedChanged += MinutesCheckedChange;
+            // 
+            // SecondsButton
+            // 
+            SecondsButton.AutoSize = true;
+            SecondsButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            SecondsButton.Location = new Point(462, 226);
+            SecondsButton.Name = "SecondsButton";
+            SecondsButton.Size = new Size(95, 29);
+            SecondsButton.TabIndex = 6;
+            SecondsButton.TabStop = true;
+            SecondsButton.Text = "Seconds";
+            SecondsButton.UseVisualStyleBackColor = true;
+            SecondsButton.CheckedChanged += SecondsCheckedChange;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Italic, GraphicsUnit.Point);
+            label2.Location = new Point(104, 171);
+            label2.Name = "label2";
+            label2.Size = new Size(204, 32);
+            label2.TabIndex = 7;
+            label2.Text = "Show the result in:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Italic, GraphicsUnit.Point);
+            label3.Location = new Point(104, 292);
+            label3.Name = "label3";
+            label3.Size = new Size(292, 32);
+            label3.TabIndex = 8;
+            label3.Text = "Left until the selected date:";
+            // 
+            // ResultLabel
+            // 
+            ResultLabel.AutoSize = true;
+            ResultLabel.Font = new Font("Segoe UI", 18F, FontStyle.Italic, GraphicsUnit.Point);
+            ResultLabel.Location = new Point(104, 350);
+            ResultLabel.Name = "ResultLabel";
+            ResultLabel.Size = new Size(76, 32);
+            ResultLabel.TabIndex = 9;
+            ResultLabel.Text = "Result";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Italic, GraphicsUnit.Point);
+            label4.Location = new Point(104, 56);
+            label4.Name = "label4";
+            label4.Size = new Size(590, 32);
+            label4.TabIndex = 10;
+            label4.Text = "Example: 15.01.1990 15:30 (hours:minutes are optional)";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(ResultLabel);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(SecondsButton);
+            Controls.Add(MinutesButton);
+            Controls.Add(DaysButton);
+            Controls.Add(MonthsButton);
+            Controls.Add(YearsButton);
+            Controls.Add(textBox);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox;
+        private RadioButton YearsButton;
+        private RadioButton MonthsButton;
+        private RadioButton DaysButton;
+        private RadioButton MinutesButton;
+        private RadioButton SecondsButton;
+        private Label label2;
+        private Label label3;
+        private Label ResultLabel;
+        private Label label4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,102 @@
+using System.Windows.Forms;
+
 namespace Running_Button
 {
     public partial class Form1 : Form
     {
+        TimeSpan? time = null;
         public Form1()
         {
             InitializeComponent();
+        }
+        private void TextChanged(object sender, EventArgs e)
+        {
+            if (textBox.Text.Length == 10 || textBox.Text.Length == 16)
+            {
+                try
+                {
+                    string minute = "", hour = "", day = "", month = "", year = "";
+                    day += textBox.Text[0];
+                    day += textBox.Text[1];
+                    month += textBox.Text[3];
+                    month += textBox.Text[4];
+                    year += textBox.Text[6];
+                    year += textBox.Text[7];
+                    year += textBox.Text[8];
+                    year += textBox.Text[9];
+                    if (textBox.Text.Length == 16)
+                    {
+                        hour += textBox.Text[11];
+                        hour += textBox.Text[12];
+                        minute += textBox.Text[14];
+                        minute += textBox.Text[15];
+                    }
+                    else
+                    {
+                        hour = "0";
+                        minute = "0";
+                    }
+                    DateTime date = new DateTime(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day), Convert.ToInt32(hour), Convert.ToInt32(minute), 0);
+                    if (date < DateTime.Now)
+                    {
+                        MessageBox.Show("You wrote time in the past!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        time = date - DateTime.Now;
+
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Wrong input!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void YearsCheckedChange(object sender, EventArgs e)
+        {
+            if (YearsButton.Checked && time != null)
+            {
+                ResultLabelShow((double)time.Value.TotalDays/365.25, "Yars"); // “ут така€ же ситуаци€, как и с мес€цем
+            }
+        }
+
+        private void MonthsCheckedChange(object sender, EventArgs e)
+        {
+            if (MonthsButton.Checked && time != null)
+            {
+                ResultLabelShow((double)time.Value.TotalDays/30.4, "Months"); // я бы мог написать огромную кучу проверок на то какие именно мес€цы прошли (28-31 дней в мес€це), но не думаю что это обходимо. ƒелю на среднее число
+
+            }
+        }
+
+        private void DaysCheckedChange(object sender, EventArgs e)
+        {
+            if (DaysButton.Checked && time != null)
+            {
+                ResultLabelShow((int)time.Value.TotalDays, "Days");
+
+            }
+        }
+
+        private void MinutesCheckedChange(object sender, EventArgs e)
+        {
+            if (MinutesButton.Checked && time != null)
+            {
+                ResultLabelShow((int)time.Value.TotalMinutes, "minutes");
+            }
+        }
+        private void SecondsCheckedChange(object sender, EventArgs e)
+        {
+            if (SecondsButton.Checked && time != null)
+            {
+                ResultLabelShow((int)time.Value.TotalSeconds, "seconds");
+            }
+        }
+        private void ResultLabelShow(double number, string variable)
+        {
+            ResultLabel.Text = number.ToString() + " " + variable;
         }
     }
 }

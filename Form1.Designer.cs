62f437e9cc95f39ccc94d6db9d86486b43881a58
Murashko Vladislav <vladislavuh2000@gmail.com>
2023-09-30 16:03:49 +0300

    The sixth task was completed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Calendar = new MonthCalendar();
+            textBox = new TextBox();
+            label = new Label();
             SuspendLayout();
+            // 
+            // Calendar
+            // 
+            Calendar.Location = new Point(555, 114);
+            Calendar.Name = "Calendar";
+            Calendar.TabIndex = 0;
+            // 
+            // textBox
+            // 
+            textBox.Location = new Point(201, 208);
+            textBox.Name = "textBox";
+            textBox.Size = new Size(299, 23);
+            textBox.TabIndex = 1;
+            textBox.TextChanged += TextBoxChanged;
+            // 
+            // label
+            // 
+            label.AutoSize = true;
+            label.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            label.Location = new Point(201, 162);
+            label.Name = "label";
+            label.Size = new Size(299, 25);
+            label.TabIndex = 2;
+            label.Text = "Enter the date. Example: 15.01.1990";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label);
+            Controls.Add(textBox);
+            Controls.Add(Calendar);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MonthCalendar Calendar;
+        private TextBox textBox;
+        private Label label;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,33 @@ namespace Running_Button
         {
             InitializeComponent();
         }
+
+        private void TextBoxChanged(object sender, EventArgs e)
+        {
+            if (textBox.Text.Length == 10)
+            {
+                string day = "", month = "", year = "";
+                try
+                {
+                    day += textBox.Text[0];
+                    day += textBox.Text[1];
+                    month += textBox.Text[3];
+                    month += textBox.Text[4];
+                    year += textBox.Text[6];
+                    year += textBox.Text[7];
+                    year += textBox.Text[8];
+                    year += textBox.Text[9];
+                    DateTime d = new DateTime(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day));
+                    Calendar.TodayDate = d;
+                    Calendar.SelectionStart = d;
+                    Calendar.SelectionEnd = d;
+                }
+                catch
+                {
+                    MessageBox.Show("Wrong input!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox.Text = "";
+                }
+            }
+        }
     }
 }
